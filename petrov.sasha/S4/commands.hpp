b7797d0e27cb973a67836e4b9f35e644e344c19a
sp0146001 <sp0146001@gmail.com>
2026-05-23 18:51:44 +0300

    change idea of realisation

--- petrov.sasha/S4/commands.hpp
+++ petrov.sasha/S4/commands.hpp
@@ -2,21 +2,24 @@
 #define COMMANDS_HPP
 
 #include <iosfwd>
+#include <string>
+#include "bstree.hpp"
 
 namespace petrov
 {
-  class Storage;
+  using InnerTree = BSTree< int, std::string >;
+  using Storage = BSTree< std::string, InnerTree >;
 
   struct Command
   {
-    const char * name;
-    void (*execute)(Storage & storage, std::istream & in, std::ostream & out);
+    const char* name;
+    void (*execute)(Storage& storage, std::istream& in, std::ostream& out);
   };
-  const Command * findCommand(const char * name) noexcept;
-  void printCommand(Storage & storage, std::istream & in, std::ostream & out);
-  void complementCommand(Storage & storage, std::istream & in, std::ostream & out);
-  void intersectCommand(Storage & storage, std::istream & in, std::ostream & out);
-  void unionCommand(Storage & storage, std::istream & in, std::ostream & out);
+  const Command* findCommand(const std::string& name);
+  void printCommand(Storage& storage, std::istream& in, std::ostream& out);
+  void complementCommand(Storage& storage, std::istream& in, std::ostream& out);
+  void intersectCommand(Storage& storage, std::istream& in, std::ostream& out);
+  void unionCommand(Storage& storage, std::istream& in, std::ostream& out);
 }
 
 #endif
