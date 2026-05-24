0f978554b4bebf719601f2e06c28800c1a7de83d
sp0146001 <sp0146001@gmail.com>
2026-05-24 17:22:50 +0300

    change type of reading file, correct reading&writing

--- petrov.sasha/S4/main.cpp
+++ petrov.sasha/S4/main.cpp
@@ -1,43 +1,52 @@
 #include "commands.hpp"
+
 #include <fstream>
 #include <iostream>
+#include <sstream>
 #include <string>
 
-int main(int argc, char * argv[])
+int main(int argc, char* argv[])
 {
-  if (argc != 2)
-  {
+  if (argc != 2) {
     std::cerr << "Invalid parameters\n";
     return 1;
   }
+
   std::ifstream file(argv[1]);
   if (!file) {
     std::cerr << "Cannot open file\n";
     return 1;
   }
   petrov::Storage storage;
-  std::string dataset;
-  while (file >> dataset) {
+  std::string line;
+  while (std::getline(file, line)) {
+    if (line.empty()) {
+      continue;
+    }
+    std::istringstream iss(line);
+    std::string dataset;
+    iss >> dataset;
     petrov::InnerTree tree;
     int key = 0;
     std::string value;
-    while (file >> key >> value) {
+    while (iss >> key >> value) {
       tree.push(key, value);
-      if (file.peek() == '\n') {
-        break;
-      }
     }
     storage.push(dataset, tree);
   }
-  std::string commandName;
-  while (std::cin >> commandName) {
-    const petrov::Command * command =
-      petrov::findCommand(commandName);
+  while (std::getline(std::cin, line)) {
+    if (line.empty()) {
+      continue;
+    }
+    std::istringstream iss(line);
+    std::string commandName;
+    iss >> commandName;
+    const petrov::Command* command = petrov::findCommand(commandName);
     if (command == nullptr) {
       std::cout << "<INVALID COMMAND>\n";
       continue;
     }
-    command->execute(storage, std::cin, std::cout);
+    command->execute(storage, iss, std::cout);
   }
   return 0;
 }
