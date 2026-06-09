2ef1da7b4ee7b3c2c9e67db1c2db286cbaaa5697
sp0146001 <sp0146001@gmail.com>
2026-06-09 17:17:05 +0300

    struct cmds - for connect asking command by user with same command in realisation(like map). And then - all commands(initializations)

--- petrov.sasha/F0/commands.hpp
+++ petrov.sasha/F0/commands.hpp
@@ -1,5 +1,5 @@
-#ifndef COMMANDS_HPP
-#define COMMANDS_HPP
+#ifndef PETROV_F0_COMMANDS_HPP
+#define PETROV_F0_COMMANDS_HPP
 
 #include <iosfwd>
 #include <string>
@@ -10,6 +10,36 @@
 namespace petrov
 {
   using Tokens = std::vector< std::string >;
+
+  struct Command
+  {
+    const char* name;
+    void (*execute)(Database& database, const Tokens& tokens, std::istream& in, std::ostream& out);
+  };
+
+  const Command* findCommand(const std::string& name);
+
+  void addQuestion(Database& database, const Tokens& tokens, std::istream& in, std::ostream& out);
+  void delQuestion(Database& database, const Tokens& tokens, std::istream& in, std::ostream& out);
+  void editQuestion(Database& database, const Tokens& tokens, std::istream& in, std::ostream& out);
+  void listQuestions(Database& database, const Tokens& tokens, std::istream& in, std::ostream& out);
+  void findQuestions(Database& database, const Tokens& tokens, std::istream& in, std::ostream& out);
+
+  void addTag(Database& database, const Tokens& tokens, std::istream& in, std::ostream& out);
+  void delTag(Database& database, const Tokens& tokens, std::istream& in, std::ostream& out);
+  void listTags(Database& database, const Tokens& tokens, std::istream& in, std::ostream& out);
+  void tagQuestion(Database& database, const Tokens& tokens, std::istream& in, std::ostream& out);
+  void untagQuestion(Database& database, const Tokens& tokens, std::istream& in, std::ostream& out);
+  void questionTags(Database& database, const Tokens& tokens, std::istream& in, std::ostream& out);
+
+  void generate(Database& database, const Tokens& tokens, std::istream& in, std::ostream& out);
+  void printTemplate(Database& database, const Tokens& tokens, std::istream& in, std::ostream& out);
+
+  void exportDb(Database& database, const Tokens& tokens, std::istream& in, std::ostream& out);
+  void importDb(Database& database, const Tokens& tokens, std::istream& in, std::ostream& out);
+
+  void statsQuestions(Database& database, const Tokens& tokens, std::istream& in, std::ostream& out);
+  void statsTags(Database& database, const Tokens& tokens, std::istream& in, std::ostream& out);
 }
 
 #endif
