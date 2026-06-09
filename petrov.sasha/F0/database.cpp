f90e61dca0c8f8683c7150e8225e34789eee79ea
sp0146001 <sp0146001@gmail.com>
2026-06-09 16:37:54 +0300

    files for main database questions, use avl, questions and tags for it

---
+++ petrov.sasha/F0/database.cpp
@@ -0,0 +1,17 @@
+#ifndef DATABASE_HPP
+#define DATABASE_HPP
+
+#include <cstddef>
+#include <string>
+
+#include "avltree.hpp"
+#include "question.hpp"
+#include "tag.hpp"
+
+namespace petrov
+{
+
+}
+
+#endif
+
