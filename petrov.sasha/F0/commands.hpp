ba4ba1e21f6ac1d71186819af7f66c902b78c74a
sp0146001 <sp0146001@gmail.com>
2026-06-09 17:14:13 +0300

    file will use database functional, tokens = parts of line-question

---
+++ petrov.sasha/F0/commands.hpp
@@ -0,0 +1,15 @@
+#ifndef COMMANDS_HPP
+#define COMMANDS_HPP
+
+#include <iosfwd>
+#include <string>
+#include <vector>
+
+#include "database.hpp"
+
+namespace petrov
+{
+  using Tokens = std::vector< std::string >;
+}
+
+#endif
