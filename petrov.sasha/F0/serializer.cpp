8f7a3599bab055247df6efe188fb2fcb3544a93a
sp0146001 <sp0146001@gmail.com>
2026-06-10 23:53:37 +0300

    export database logic - from Tags to Questions

--- petrov.sasha/F0/serializer.cpp
+++ petrov.sasha/F0/serializer.cpp
@@ -37,3 +37,39 @@ static bool readCount(std::istream& in, std::size_t& out)
   return true;
 }
 
+bool petrov::exportDatabase(const std::string& path, const Database& database)
+{
+  std::ofstream file(path);
+  if (!file) {
+    return false;
+  }
+  file << "TAGS " << database.tags.size() << '\n';
+  for (TagTree::const_iterator it = database.tags.begin(); it != database.tags.end(); ++it) {
+    file << it->first << '\n';
+  }
+  file << "QUESTIONS " << database.questions.size() << '\n';
+  for (QuestionTree::const_iterator it = database.questions.begin(); it != database.questions.end(); ++it) {
+    const Question& question = it->second;
+    file << typeName(question) << '\n';
+    file << question.id << '\n';
+    file << question.text << '\n';
+    file << question.options.size() << '\n';
+    for (std::size_t i = 0; i < question.options.size(); ++i) {
+      file << question.options[i] << '\n';
+    }
+    if (question.type == QuestionType::matching) {
+      for (std::size_t i = 0; i < question.rights.size(); ++i) {
+        file << question.rights[i] << '\n';
+      }
+    } else {
+      file << question.correct.size() << '\n';
+      for (std::size_t i = 0; i < question.correct.size(); ++i) {
+        file << question.correct[i] << '\n';
+      }
+    }
+    writeQuestionTags(file, question);
+    file << "END\n";
+  }
+  return static_cast< bool >(file);
+}
+
