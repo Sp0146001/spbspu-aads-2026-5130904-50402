c68296b195f04bd0f1259ffd756d1e617f8f7d40
sp0146001 <sp0146001@gmail.com>
2026-06-18 19:57:24 +0300

    tests for new functions

--- petrov.sasha/S1/test-list.cpp
+++ petrov.sasha/S1/test-list.cpp
@@ -1,8 +1,5 @@
 #include <boost/test/unit_test.hpp>
 
-#include <sstream>
-#include <string>
-#include <utility>
 #include "list.hpp"
 
 BOOST_AUTO_TEST_SUITE(PetrovListTests)
@@ -174,7 +171,7 @@ BOOST_AUTO_TEST_CASE(test_const_iterator)
   list.push_back(1);
   list.push_back(2);
 
-  const petrov::List< int > & const_list = list;
+  const petrov::List< int >& const_list = list;
   petrov::LCIter< int > it = const_list.cbegin();
 
   BOOST_CHECK_EQUAL(*it, 1);
@@ -198,6 +195,124 @@ BOOST_AUTO_TEST_CASE(test_swap)
   BOOST_CHECK_EQUAL(list1.front(), 2);
   BOOST_CHECK_EQUAL(list2.size(), 1);
   BOOST_CHECK_EQUAL(list2.front(), 1);
+}
+
+BOOST_AUTO_TEST_CASE(test_splice)
+{
+  petrov::List< int > list1;
+  list1.push_back(1);
+  list1.push_back(2);
+
+  petrov::List< int > list2;
+  list2.push_back(10);
+  list2.push_back(20);
+  list2.push_back(30);
+
+  // splice(pos, other&&) - перенос всех через rvalue
+  petrov::List< int > tmp_rvalue;
+  tmp_rvalue.push_back(100);
+  tmp_rvalue.push_back(200);
+  tmp_rvalue.push_back(300);
+  list1.splice(list1.end(), std::move(tmp_rvalue));
+  BOOST_CHECK_EQUAL(list1.size(), 5);
+  BOOST_CHECK(tmp_rvalue.empty());
+  petrov::LIter< int > check = list1.begin();
+  BOOST_CHECK_EQUAL(*check++, 1);
+  BOOST_CHECK_EQUAL(*check++, 2);
+  BOOST_CHECK_EQUAL(*check++, 100);
+  BOOST_CHECK_EQUAL(*check++, 200);
+  BOOST_CHECK_EQUAL(*check, 300);
+  list1.popBack();
+  list1.popBack();
+  list1.popBack();
+
+  // splice(pos, other) - перенос всех элементов
+  petrov::List< int > tmp1;
+  tmp1.push_back(100);
+  tmp1.push_back(200);
+  list1.splice(list1.end(), tmp1);
+  BOOST_CHECK_EQUAL(list1.size(), 4);
+  BOOST_CHECK_EQUAL(tmp1.size(), 0);
+
+  // splice(pos, other, i) - перенос одного элемента
+  petrov::LIter< int > it2 = list2.begin();
+  ++it2;
+  list1.splice(list1.begin(), list2, it2);
+  BOOST_CHECK_EQUAL(list1.size(), 5);
+  BOOST_CHECK_EQUAL(list2.size(), 2);
+
+  // splice(pos, other, first, last) - перенос диапазона
+  petrov::LIter< int > first = list2.begin();
+  petrov::LIter< int > last = list2.begin();
+  ++last;
+  list1.splice(list1.end(), list2, first, last);
+  BOOST_CHECK_EQUAL(list1.size(), 6);
+  BOOST_CHECK_EQUAL(list2.size(), 1);
+}
+
+BOOST_AUTO_TEST_CASE(test_sort)
+{
+  petrov::List< int > list;
+  list.push_back(5);
+  list.push_back(2);
+  list.push_back(8);
+  list.push_back(1);
+  list.push_back(4);
+
+  list.sort();
+
+  petrov::LIter< int > it = list.begin();
+  BOOST_CHECK_EQUAL(*it++, 1);
+  BOOST_CHECK_EQUAL(*it++, 2);
+  BOOST_CHECK_EQUAL(*it++, 4);
+  BOOST_CHECK_EQUAL(*it++, 5);
+  BOOST_CHECK_EQUAL(*it, 8);
+}
+
+BOOST_AUTO_TEST_CASE(test_merge)
+{
+  petrov::List< int > list1;
+  list1.push_back(1);
+  list1.push_back(3);
+  list1.push_back(5);
+
+  petrov::List< int > list2;
+  list2.push_back(2);
+  list2.push_back(4);
+  list2.push_back(6);
+
+  list1.merge(list2);
+
+  BOOST_CHECK_EQUAL(list1.size(), 6);
+  BOOST_CHECK_EQUAL(list2.size(), 0);
+
+  petrov::LIter< int > it = list1.begin();
+  for (int i = 1; i <= 6; ++i) {
+    BOOST_CHECK_EQUAL(*it++, i);
+  }
+}
+
+BOOST_AUTO_TEST_CASE(test_partition)
+{
+  petrov::List< int > list;
+  list.push_back(1);
+  list.push_back(2);
+  list.push_back(3);
+  list.push_back(4);
+  list.push_back(5);
+
+  petrov::LIter< int > split = list.partition([](int v) {
+    return v % 2 == 0;
+  });
+
+  BOOST_CHECK_EQUAL(*split, 1);
+
+  petrov::LIter< int > it = list.begin();
+  BOOST_CHECK_EQUAL(*it++, 2);
+  BOOST_CHECK_EQUAL(*it++, 4);
+  BOOST_CHECK_EQUAL(*it++, 1);
+  BOOST_CHECK_EQUAL(*it++, 3);
+  BOOST_CHECK_EQUAL(*it, 5);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
